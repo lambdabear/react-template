d0d85d5d8ad26bda503b6bef5ebd4ba569f09863
Ethan Zhang <bluefurbear@gmail.com>
2020-02-20 05:38:03 +0800

    add temperature and humidity

--- src/pages/Home/BaseStation.js
+++ src/pages/Home/BaseStation.js
@@ -26,12 +26,14 @@ function BaseStation() {
     battery: 0, // /1000 = 电量
     operator: 0, // 运营商编码
     ipAddr: '0.0.0.0',
-    datetime: '2019-01-01 00:00:00',
+    updateTime: '2019-01-01 00:00:00',
+    temperature: 0.0,
+    humidity: 0.0,
     devices: [],
   });
   useEffect(() => {
-    const ws = new WebSocket('ws://52.83.48.13:8080/ws/');
-    setTimeout(() => {
+    const ws = new WebSocket('ws://52.83.48.13:8088/ws/');
+    setInterval(() => {
       if (ws.readyState === WebSocket.OPEN) {
         ws.send('pong');
       }
@@ -52,22 +54,25 @@ function BaseStation() {
   return (
     <div>
       <Descriptions title="LoRa-NB 网关" bordered>
-        <Descriptions.Item label="网关ID">{data.gatewayID}</Descriptions.Item>
+        <Descriptions.Item label="网关ID">{data.gatewayID.substring(16)}</Descriptions.Item>
         <Descriptions.Item label="信号强度">{data.rssi}</Descriptions.Item>
-        <Descriptions.Item label="电压">{`${(data.battery / 1000).toFixed(3)} v`}</Descriptions.Item>
-        <Descriptions.Item label="运营商编码">{data.operator}</Descriptions.Item>
+        <Descriptions.Item label="电压">{`${(data.battery / 1000).toFixed(2)} V`}</Descriptions.Item>
+        <Descriptions.Item label="电量">{`${(((data.battery / 1000) - 3.0) / 0.6 * 100).toFixed(0)} %`}</Descriptions.Item>
+        <Descriptions.Item label="温度">{`${data.temperature.toFixed(1)} °C`}</Descriptions.Item>
+        <Descriptions.Item label="湿度">{`${data.humidity.toFixed(1)} %`}</Descriptions.Item>
+        <Descriptions.Item label="运营商">{data.operator === 46011 ? `电信` : `移动`}</Descriptions.Item>
         <Descriptions.Item label="IP地址">{data.ipAddr}</Descriptions.Item>
-        <Descriptions.Item label="更新时间" span={2}>{data.datetime}</Descriptions.Item>
+        <Descriptions.Item label="更新时间" span={2}>{data.updateTime}</Descriptions.Item>
         <Descriptions.Item label="状态" span={3}>
           <Badge
-            status={data.datetime ? 'success' : 'default'}
-            text={data.datetime ? '在线' : '离线'}
+            status={data.updateTime ? 'success' : 'default'}
+            text={data.updateTime ? '在线' : '离线'}
           />
         </Descriptions.Item>
         {
           data.devices.map((e, index) => Object.keys(e).map((key) => (
             <Descriptions.Item label={deviceCfg[key](index + 1)}>
-              {e[key]}
+              {key === 'data' ? `${e[key].toFixed(2)} MPa` : (key === 'battery' ? `${e[key]} %` : e[key])}
             </Descriptions.Item>
           )))
         }
